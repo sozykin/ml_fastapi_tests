f5f429c886ff7a915ced1a42decbfdd1d55cbba1
Andrey Sozykin <sozykin@gmail.com>
2021-12-28 19:06:48 +0500

    added test_main.py

---
+++ test_main.py
@@ -0,0 +1,29 @@
+from fastapi.testclient import TestClient
+import json
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+def test_read_predict_positive():
+    response = client.post("/predict/",
+        json={"text": "I like machine learning"}
+    )
+    json_data = response.json() 
+
+    assert response.status_code == 200
+    assert json_data['label'] == 'POSITIVE'
+
+def test_read_predict_negative():
+    response = client.post("/predict/",
+        json={"text": "I hate machine learning"}
+    )
+    json_data = response.json() 
+
+    assert response.status_code == 200
+    assert json_data['label'] == 'NEGATIVE'
