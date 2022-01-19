d19c34203db049c35e16b1cf87d7330b149b9000
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-01-19 16:08:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from fastapi import FastAPI
 from transformers import pipeline
-from pydantic import BaseModel
+import torch
+from transformers import AutoModelForSequenceClassification
+from transformers import BertTokenizerFast
 
-class Item(BaseModel):
-    text: str
+#class Item(BaseModel):
+    text: "Не важно.Всё нормально"
 
 app = FastAPI()
-classifier = pipeline("sentiment-analysis")
+#classifier = pipeline("sentiment-analysis")
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello UrFU"}
 
 @app.post("/predict/")
-def predict(item: Item):
-    return classifier(item.text)[0]
+#def predict(item: Item):
+    #return classifier(item.text)[0]
+    
+tokenizer = BertTokenizerFast.from_pretrained('blanchefort/rubert-base-cased-sentiment-rusentiment')
+model = AutoModelForSequenceClassification.from_pretrained('blanchefort/rubert-base-cased-sentiment-rusentiment', return_dict=True)
+
+@torch.no_grad()
+def predict(text):
+    inputs = tokenizer(text, max_length=512, padding=True, truncation=True, return_tensors='pt')
+    outputs = model(**inputs)
+    predicted = torch.nn.functional.softmax(outputs.logits, dim=1)
+    predicted = torch.argmax(predicted, dim=1).numpy()
+    return     
+   
