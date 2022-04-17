432152f97f049bd897685ce59dd066fe4c556d88
Andrey Sozykin <sozykin@gmail.com>
2022-04-17 21:25:30 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -9,7 +9,7 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
-def test_read_predict_positive():
+def test_predict_positive():
     response = client.post("/predict/",
         json={"text": "I like machine learning!"}
     )
@@ -18,7 +18,7 @@ def test_read_predict_positive():
     assert response.status_code == 200
     assert json_data['label'] == 'POSITIVE'
 
-def test_read_predict_negative():
+def test_predict_negative():
     response = client.post("/predict/",
         json={"text": "I hate machine learning!"}
     )
