e8c23a20652ef31f8aca5ad5fd1247febfb9ab82
ivan <team-on-in@mail.ru>
2024-04-07 20:21:15 +0300

    Добавлена  документация

--- main.py
+++ main.py
@@ -2,20 +2,18 @@ from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
-
 class Item(BaseModel):
     text: str
-
 
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
-
 @app.get("/")
 def root():
+    """Корневой маршрут возвращает сообщение "Hello World"."""
     return {"message": "Hello World"}
-
 
 @app.post("/predict/")
 def predict(item: Item):
+    """Принимает JSON с текстом и возвращает предсказание настроения."""
     return classifier(item.text)[0]
